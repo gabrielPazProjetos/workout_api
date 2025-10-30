f636e800dd1b01d93763f5963e9ea0968227f35f
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-10-30 12:26:40 -0300

    Update atleta.py

--- app/models/atleta.py
+++ app/models/atleta.py
@@ -1,1 +1,12 @@
+from sqlalchemy import Column, Integer, String, UniqueConstraint
+from app.database import Base
 
+class Atleta(Base):
+    __tablename__ = "atletas"
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, nullable=False)
+    cpf = Column(String, unique=True, nullable=False)
+    centro_treinamento = Column(String, nullable=False)
+    categoria = Column(String, nullable=False)
+
+    __table_args__ = (UniqueConstraint("cpf", name="uq_atleta_cpf"),)
