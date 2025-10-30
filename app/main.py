aa9691035b43475c77bc61f5d4ec74b4c30df9a9
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-10-30 12:25:25 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
+from fastapi import FastAPI
+from app.routers import atleta
+from fastapi_pagination import add_pagination
 
+app = FastAPI(title="Workout API")
+
+app.include_router(atleta.router, prefix="/atletas", tags=["Atletas"])
+
+add_pagination(app)
