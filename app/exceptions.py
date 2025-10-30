614d84cfa321cee02a8c2e4f3ff2302d91611b7c
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-10-30 12:27:32 -0300

    Update exceptions.py

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,1 +1,7 @@
+from fastapi import HTTPException
 
+def atleta_cpf_duplicado(cpf: str):
+    raise HTTPException(
+        status_code=303,
+        detail=f"Já existe um atleta cadastrado com o cpf: {cpf}"
+    )
